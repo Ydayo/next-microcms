ff48163dde8d32b95bc1c6c0064fc6ab17ad113b
IshikawaTokyo <yyusb.mac@gmail.com>
2023-11-23 20:51:04 +0900

    done

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,10 +1,20 @@
 import React, { FC } from "react";
 import styles from "./Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer: FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles["footer-title"]}>FooterTitle</div>
+      <Link href={"/"}>
+        <Image
+          src={"/ishikawa-tokyo-logo-white-transparent.png"}
+          alt="logo"
+          width={500}
+          height={500}
+          className={styles["header-logo"]}
+        />
+      </Link>
     </footer>
   );
 };
