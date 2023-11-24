7506a66eb516bb8e48b0c9b9897bda6a92a18b4e
IshikawaTokyo <yyusb.mac@gmail.com>
2023-11-24 11:24:44 +0900

    done

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import Header from "../../ui/Header/Header";
-import Skill from "../../ui/Skill/Skill";
-import { Portfolio } from "../../ui/Portfolio/Portfolio";
-import SeeMore from "../../ui/SeeMore/SeeMore";
-import Footer from "../../ui/Footer/Footer";
+import Header from "@/components/ui/Header/Header";
+import Skill from "@/components/ui/Skill/Skill";
+import { Portfolio } from "@/components/ui/Portfolio/Portfolio";
+import SeeMore from "@/components/ui/SeeMore/SeeMore";
+import Footer from "@/components/ui/Footer/Footer";
 import styles from "@/styles/page.module.css";
 
 const Top = () => {
